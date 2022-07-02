309973cc369032d07dd582270a99d12fe8b5e079
sakuoden <4oden18@gmail.com>
2022-07-02 22:50:56 +0900

    ホーム画面仮実装。

---
+++ src/main/java/sample/presentation/HomeController.java
@@ -0,0 +1,14 @@
+package sample.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    String ホーム画面() {
+        return "home";
+    }
+}
