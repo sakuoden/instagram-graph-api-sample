150e8528addfc25014e249f4657901162450b744
sakuoden <4oden18@gmail.com>
2022-07-03 16:16:55 +0900

    @Controllerのつけ忘れ

--- src/main/java/sample/presentation/LoginController.java
+++ src/main/java/sample/presentation/LoginController.java
@@ -1,8 +1,10 @@
 package sample.presentation;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/login")
 public class LoginController {
     @GetMapping
