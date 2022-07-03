106f07bdf965899fda81ab39825ad8a5418efffc
sakuoden <4oden18@gmail.com>
2022-07-03 16:32:31 +0900

    FacebookログインをするためのSpring SecurityのFilterへのリクエスト先を誤っていたので修正する。

--- src/main/java/sample/presentation/LoginController.java
+++ src/main/java/sample/presentation/LoginController.java
@@ -1,6 +1,8 @@
 package sample.presentation;
 
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/login")
 public class LoginController {
     @GetMapping
-    String ログイン画面() {
+    String ログイン画面(Model model) {
+        String requestUrl = OAuth2AuthorizationRequestRedirectFilter.DEFAULT_AUTHORIZATION_REQUEST_BASE_URI + "/facebook";
+        model.addAttribute("requestUrl", requestUrl);
         return "login";
     }
 }
