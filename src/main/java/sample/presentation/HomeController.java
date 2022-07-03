379fb577ede53964584c67a354d7db75af701d3b
sakuoden <4oden18@gmail.com>
2022-07-03 23:53:18 +0900

    Instagramの投稿一覧を表示するホーム画面を作成する。

--- src/main/java/sample/presentation/HomeController.java
+++ src/main/java/sample/presentation/HomeController.java
@@ -1,14 +1,26 @@
 package sample.presentation;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sample.application.InstagramService;
+import sample.infrastructure.api.response.media.MediasResponse;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    InstagramService instagramService;
+
     @GetMapping
-    String ホーム画面() {
+    String ホーム画面(Model model) {
+        MediasResponse medias = instagramService.medias();
+        model.addAttribute("mediaList", medias.data());
+
         return "home";
+    }
+
+    HomeController(InstagramService instagramService) {
+        this.instagramService = instagramService;
     }
 }
